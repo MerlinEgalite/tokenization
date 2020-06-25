46bc7586be9166fe6582d4060c70348c0da1c09b
MerlinEgalite <egalite.merlin@gmail.com>
2020-06-25 19:31:38 +0200

    add some tests

---
+++ test/MyToken.test.js
@@ -0,0 +1,26 @@
+const Token = artifacts.require("MyToken");
+
+var chai = require("chai");
+
+const BN = web3.utils.BN;
+const chaiBN = require("chai-bn")(BN);
+chai.use(chaiBN);
+
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+contract("Token Test", async accounts => {
+    const [ initialHolder, recipient, anotherAccount ] = accounts;
+
+    it("All tokens should be in my account", async () => {
+        let instance = await Token.deployed();
+        let totalSupply = await instance.totalSupply();
+        //old style:
+        //let balance = await instance.balanceOf.call(initialHolder);
+        //assert.equal(balance.valueOf(), 0, "Accounts 1 has a balance");
+        //condensed, easier readable styke:
+        expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
+    });
+});
