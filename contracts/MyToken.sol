20b4bd917c2abe4ee5a83d199d69826b63796ebf
MerlinEgalite <egalite.merlin@gmail.com>
2020-06-25 18:46:56 +0200

    fix issues with solidity version and ERC20 tokens

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,10 +1,9 @@
 pragma solidity >=0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("StarDucks Capuccino", "SCT", 0) public {
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("StarDucks Capuccino", "SCT") public {
         _mint(msg.sender, initialSupply);
     }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: "0.6.1"
+      version: "0.6.2"
     }
   }
 };
