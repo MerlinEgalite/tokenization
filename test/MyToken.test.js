c29b4982068933d7048eafbfd4942ea48d736563
MerlinEgalite <egalite.merlin@gmail.com>
2020-06-25 19:59:01 +0200

    add some tests

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -23,4 +23,23 @@ contract("Token Test", async accounts => {
         //condensed, easier readable styke:
         expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
     });
+
+    it("I can send tokens from Account 1 to Account 2", async () => {
+        const sendTokens = 1;
+        let instance = await Token.deployed();
+        let totalSupply = await instance.totalSupply();
+        expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
+        expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
+        expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
+        expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+    });
+
+    it("It's not possible to send more tokens than Account 1 has", async () => {
+        let instance = await Token.deployed();
+        let balanceOfAccount = await instance.balanceOf(initialHolder);
+
+        expect(instance.transfer(recipient, new BN(balanceOfAccount+1))).to.eventually.be.rejected;
+        //check if the balance is still the same
+        expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(new BN(balanceOfAccount));
+    });
 });
