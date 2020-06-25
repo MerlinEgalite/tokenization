cb77aa77d3480bfb561f06d5178d95dc58cf4748
MerlinEgalite <egalite.merlin@gmail.com>
2020-06-25 21:26:10 +0200

    add crowdsale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var MyToken = artifacts.require("./MyToken.sol");
+var MyTokenSales = artifacts.require("./MyTokenSale.sol");
 
 module.exports = async function(deployer) {
-  deployer.deploy(MyToken, 10000000);
+  let addr = await web3.eth.getAccounts();
+  await deployer.deploy(MyToken, 10000000);
+  await deployer.deploy(MyTokenSales, 1, addr[0], MyToken.address);
+  let tokenInstance = await MyToken.deployed();
+  await tokenInstance.transfer(MyTokenSales.address, 10000000);
 };
